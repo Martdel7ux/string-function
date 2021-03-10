66353f2e7eb9cd0e02789cd4737a0798633b0298
Martin Ahoto <martinahoto4@gmail.com>
2021-03-10 19:14:35 +0200

    Initial Commit

---
+++ string function/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  string function
+//
+//  Created by MARTIN on 10/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
